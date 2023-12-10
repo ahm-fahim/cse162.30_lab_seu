929b0eb0fd1a65fa52bc1d51a49c5f744876cc6b
ahm-fahim <contactahmfahim@gmail.com>
2023-12-10 11:54:28 +0600

    class 5

---
+++ sm.c
@@ -0,0 +1,19 @@
+
+// int a, b; // a , b = garbage value
+// a = b;    // a = garbage value
+
+// if (a - b) // 0 - false
+//     printf("Good Morning\n");
+// if (a <= b) // 1 - true
+//     printf("Welcome to C exam\n");
+// if (a = 1) // 1 - true
+//     printf("A is true\n");
+// else if (a == 1) // 0 - false
+//     printf("A is also true\n");
+// b = a - 1; // garbage
+// if (b)     // false
+//     printf("The end\n");
+// else // true
+//     printf("You get A\n");
+
+// printf("%d", a < b); // always true
