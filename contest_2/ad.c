6a9571f7133cadffcfc252244429eee2b0ceb9f8
ahm-fahim <contactahmfahim@gmail.com>
2024-01-06 19:52:37 +0600

    solved

--- contest_2/ad.c
+++ contest_2/ad.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    int n;
-   
+int main()
+{
+    char s[4];
+    scanf("%s", &s);
 
+    int len = strlen(s);
+
+    if(len==1)
+        printf("%s%s%s%s%s%s", s,s,s,s,s,s);
+    else if(len==2)    
+        printf("%s%s%s",s,s,s);
+    else
+        printf("%s%s",s,s);
+    
     return 0;
 }
